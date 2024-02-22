bf0cad75ffbcdee702331bf4a2371c53da32e9d3
Wilsonabdiel <wilsonabdiel86@gmail.com>
2024-02-22 17:05:55 +0100

    wip

--- which_anime.py
+++ which_anime.py
@@ -1,26 +1,60 @@
-from urllib.request import urlopen as uReq
+import requests
 from bs4 import BeautifulSoup as soup
-import pandas as pdp
+import pandas as pd
 
-
-class comparer:
-    def __init__(self) -> None:
+class Comparer:
+    def __init__(self):
         self.base_url = "https://www.imdb.com/"
-        
 
     def get_data(self, title):
-
         response = requests.get(self.base_url + "search/title/?genres=anime&title=" + title)
-
-        page_soup = Soup(response.text, 'html.parser')
+        page_soup = soup(response.text, 'html.parser')
+        # Extract the necessary data from page_soup
+        # This will depend on the structure of the webpage
+        # Return the data as a pandas DataFrame
+        rating_div = page_soup.find('div', class_='ratingValue')
+    # Return the data as a pandas DataFrame
+        if rating_div is not None:
+             rating = rating_div.text
+        else:
+            rating = None
+        return pd.DataFrame({'title': [title], 'rating': [rating]})
 
     def compare(self, anime_list):
-        # Use the get_anime_data method to get data for each anime
-        # Compare the data
-        # This could involve looking at ratings, number of reviews, etc.
-        # Return the comparison results
+        data_frames = [self.get_data(anime) for anime in anime_list]
+        # Here's where you'd implement your comparison logic
+        # For example, you might want to compare the average ratings of the animes
+        avg_ratings = [df['rating'].mean() for df in data_frames]
+        comparison_results = pd.DataFrame({
+            'anime': anime_list,
+            'avg_rating': avg_ratings
+        })
+        return comparison_results
 
-    if __name__ == "__main__":
-        comparer = AnimeComparer()
-        anime_list = ["Naruto", "Bleach", "One Piece"]  # Add the animes you want to compare
-        print(comparer.compare_animes(anime_list))
+class Searcher:
+    def __init__(self):
+        self.base_url = "https://www.imdb.com/"
+
+    def search(self, title):
+        response = requests.get(self.base_url + "search/title/?genres=anime&title=" + title)
+        page_soup = soup(response.text, 'html.parser')
+        watch_trailler_div = page_soup.find('a', class_='ipc-lockup-overlay ipc-focusable')
+        rate = page_soup.find('div', class_ ='sc-e-7b78b2c-5 dWdOca')
+        if watch_trailler_div is not None:
+            watch_trailler = watch_trailler_div.get('href')
+        else:
+            watch_trailler = None
+        return{
+             'title': title,
+            'watch_link': self.base_url + watch_trailler,
+            'rate_link': self.base_url + rate
+        }
+if __name__ == "__main__":
+    searcher = Searcher()
+    print(searcher.search("Naruto"))
+
+
+# if __name__ == "__main__":
+#     comparer = Comparer()
+#     anime_list = ["Naruto", "Bleach", "One Piece"]  # Add the animes you want to compare
+#     print(comparer.compare(anime_list))
